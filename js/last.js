d045201428e0759b8947c7e4e182c4cd53fe28be
Jake Ledoux <contactjakeledoux@gmail.com>
2023-01-21 08:07:17 -0500

    swap key/secret
    now using the last.fm desktop app one since it's publicly available and
    last.fm's API situation doesn't really allow for me to: A) make my app more
    efficient (w/ batched calls) or B) upgrade my api key to support more requests.

--- js/last.js
+++ js/last.js
@@ -1,5 +1,5 @@
-const LAST_KEY = "d3e73d02c31ec98e01bedf4d8306497a";
-const LAST_SECRET = "e8631552b4a07b5f7994961169da48fb";
+const LAST_KEY = "9e89b44de1ff37c5246ad0af18406454";
+const LAST_SECRET = "147320ea9b8930fe196a4231da50ada4";
 var LAST_SESSION = "";
 var LOVED_TRACKS = [];
 
@@ -13,7 +13,7 @@ function writeLovedTracks(tracks) {
     LOVED_TRACKS = [];
     for (key in tracks) {
         let track = tracks[key];
-        LOVED_TRACKS.push({ "title": track.name, "artist": track.artist.name });
+        LOVED_TRACKS.push({"title": track.name, "artist": track.artist.name});
     }
 }
 
@@ -31,31 +31,32 @@ function sign(parameters) {
 
 function getURL(parameters, signed = true) {
     let param_string = "";
-    for (key in parameters)
-    {
+    for (key in parameters) {
         param_string += `${key}=${parameters[key]}&`;
     }
     if (signed) {
         param_string += `api_sig=${sign(parameters)}&`;
     }
-    let url = "https://ws.audioscrobbler.com/2.0/?" + param_string + "format=json";
+    let url =
+        "https://ws.audioscrobbler.com/2.0/?" + param_string + "format=json";
     return url;
 }
 
 // High level  -----------------------------------------
 
 function getMobileSession(username, password) {
-    $.post(
-    getURL({
-        "method": "auth.getMobileSession",
-        "username": username,
-        "password": password,
-        "api_key": LAST_KEY
-    })).done(function(data) {
-        writeSession(data.session.key);
-    }).fail(function(data) {
-        console.warn("Last.fm authentication failed.")
-    });
+    $.post(getURL({
+         "method": "auth.getMobileSession",
+         "username": username,
+         "password": password,
+         "api_key": LAST_KEY
+     }))
+        .done(function(data) {
+            writeSession(data.session.key);
+        })
+        .fail(function(data) {
+            console.warn("Last.fm authentication failed.")
+        });
 }
 
 function loveTrack(title, artist) {
@@ -63,18 +64,19 @@ function loveTrack(title, artist) {
         console.warn("Failed to love track, session key not available.");
         return false;
     }
-    $.post(
-    getURL({
-        "method": "track.love",
-        "track": title,
-        "artist": artist,
-        "api_key": LAST_KEY,
-        "sk": LAST_SESSION
-    })).fail(function (data) {
-        console.warn("Failed to love track.", data)
-    }).done(function() {
-        LOVED_TRACKS.push({'title': title, 'artist': artist});
-    });
+    $.post(getURL({
+         "method": "track.love",
+         "track": title,
+         "artist": artist,
+         "api_key": LAST_KEY,
+         "sk": LAST_SESSION
+     }))
+        .fail(function(data) {
+            console.warn("Failed to love track.", data)
+        })
+        .done(function() {
+            LOVED_TRACKS.push({'title': title, 'artist': artist});
+        });
 }
 
 function unLoveTrack(title, artist) {
@@ -82,30 +84,34 @@ function unLoveTrack(title, artist) {
         console.warn("Failed to un-love track, session key not available.");
         return false;
     }
-    $.post(
-        getURL({
-            "method": "track.unlove",
-            "track": title,
-            "artist": artist,
-            "api_key": LAST_KEY,
-            "sk": LAST_SESSION
-        })).fail(function (data) {
+    $.post(getURL({
+         "method": "track.unlove",
+         "track": title,
+         "artist": artist,
+         "api_key": LAST_KEY,
+         "sk": LAST_SESSION
+     }))
+        .fail(function(data) {
             console.warn("Failed to un-love track.", data)
-        }).done(function () {
+        })
+        .done(function() {
             LOVED_TRACKS.pop(getLovedIndex(title, artist));
         });
 }
 
 function getLovedTracks(username) {
-    $.post(
-        getURL({
-            "method": "user.getLovedTracks",
-            "limit": 1000,
-            "user": username,
-            "api_key": LAST_KEY
-        }, false)).fail(function (data) {
+    $.post(getURL(
+               {
+                   "method": "user.getLovedTracks",
+                   "limit": 1000,
+                   "user": username,
+                   "api_key": LAST_KEY
+               },
+               false))
+        .fail(function(data) {
             console.warn("Failed to get loved tracks.", data)
-        }).done(function (data) {
+        })
+        .done(function(data) {
             writeLovedTracks(data.lovedtracks.track);
         });
 }
@@ -130,31 +136,37 @@ function getLovedIndex(title, artist) {
     return false;
 }
 
-function getFriends(username, callback, limit=15) {
-    $.post(
-        getURL({
-            "method": "user.getFriends",
-            "user": username,
-            "api_key": LAST_KEY
-        }, false)).fail(function (data) {
+function getFriends(username, callback, limit = 15) {
+    $.post(getURL(
+               {
+                   "method": "user.getFriends",
+                   "user": username,
+                   "api_key": LAST_KEY
+               },
+               false))
+        .fail(function(data) {
             console.warn("Failed to get friends.", data)
             setErrorMessage("no-friends");
-        }).done(function(data) {
+        })
+        .done(function(data) {
             callback(data);
         });
 }
 
-function getNowPlaying(username, callback, checkLength=false) {
-    $.post(
-        getURL({
-            "method": "user.getRecentTracks",
-            "user": username,
-            "limit": 1,
-            "api_key": LAST_KEY
-        }, false)).fail(function (data) {
+function getNowPlaying(username, callback, checkLength = false) {
+    $.post(getURL(
+               {
+                   "method": "user.getRecentTracks",
+                   "user": username,
+                   "limit": 1,
+                   "api_key": LAST_KEY
+               },
+               false))
+        .fail(function(data) {
             console.warn("Failed to get now playing.", data)
             setErrorMessage("connection");
-        }).done(function (data) {
+        })
+        .done(function(data) {
             try {
                 let track = data.recenttracks.track[0];
                 setErrorMessage(false);
@@ -162,16 +174,34 @@ function getNowPlaying(username, callback, checkLength=false) {
                 let artwork = track.image[3]['#text'];
                 let artistHref = track.url.split("/").slice(0, -2).join("/")
                 if (track.date == undefined)
-                    track.date = {'uts': 99999999999};
+                track.date = {'uts': 99999999999};
                 if (!currentlyPlaying && checkLength) {
-                    getLength(track.name, track.artist['#text'], function (duration) {
-                        currentlyPlaying = (parseInt(track.date.uts*1000) + duration > Date.now());
-                        callback(track.name, track.artist['#text'], currentlyPlaying, track.date.uts, artwork, track.url, artistHref);
-                    })
-                }
-                else
-                    callback(track.name, track.artist['#text'], currentlyPlaying, track.date.uts, artwork, track.url, artistHref);
-            } catch(err) {
+                    getLength(
+                        track.name,
+                        track.artist['#text'],
+                        function(duration) {
+                            currentlyPlaying =
+                                (parseInt(track.date.uts * 1000) + duration
+                                 > Date.now());
+                            callback(
+                                track.name,
+                                track.artist['#text'],
+                                currentlyPlaying,
+                                track.date.uts,
+                                artwork,
+                                track.url,
+                                artistHref);
+                        })
+                } else
+                    callback(
+                        track.name,
+                        track.artist['#text'],
+                        currentlyPlaying,
+                        track.date.uts,
+                        artwork,
+                        track.url,
+                        artistHref);
+            } catch (err) {
                 setErrorMessage("connection");
                 console.warn("Failed to get now playing.", err, data);
             }
@@ -181,42 +211,45 @@ function getNowPlaying(username, callback, checkLength=false) {
 function getLength(track, artist, callback) {
     let duration = localStorage[`<${track}><${artist}>`] || "UNKNOWN";
     if (duration == "UNKNOWN") {
-    // } if (true) {
-        $.post(
-            getURL({
-                "method": "track.getInfo",
-                "track": track,
-                "artist": artist,
-                "api_key": LAST_KEY
-            }, false)).fail(function (data) {
+        // } if (true) {
+        $.post(getURL(
+                   {
+                       "method": "track.getInfo",
+                       "track": track,
+                       "artist": artist,
+                       "api_key": LAST_KEY
+                   },
+                   false))
+            .fail(function(data) {
                 console.warn("Failed to get length.", data)
-            }).done(function (data) {
+            })
+            .done(function(data) {
                 if (data.error == undefined) {
                     // Cache duration
                     let duration = parseInt(data.track.duration);
                     localStorage[`<${track}><${artist}>`] = duration
                     callback(duration);
-                }
-                else
+                } else
                     callback(0);
             });
-    }
-    else {
+    } else {
         callback(parseInt(duration));
     }
-
 }
 
 function getInfo(username, callback) {
-    $.post(
-        getURL({
-            "method": "user.getInfo",
-            "username": username,
-            "api_key": LAST_KEY
-        }, false)).fail(function (data) {
+    $.post(getURL(
+               {
+                   "method": "user.getInfo",
+                   "username": username,
+                   "api_key": LAST_KEY
+               },
+               false))
+        .fail(function(data) {
             console.warn("Failed to get user info.", data)
             setErrorMessage("connection");
-        }).done(function (data) {
+        })
+        .done(function(data) {
             callback(data);
         });
 }
